a055fd42aafb5e790b04c35eb90463a7577f77fc
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-08-12 13:49:12 +0200

    Finishing basic landing (before hodilays!)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,10 @@ import { Open_Sans } from "next/font/google";
 const inter = Open_Sans({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "NX - Boostez votre carriere de developpeur avec des cours en ligne modernes.",
-  description: "Améliorez vos compétences en ingénierie informatique avec des cours modernes et complets.",
+  title:
+    "NX - Boostez votre carriere de developpeur avec des cours en ligne modernes.",
+  description:
+    "Améliorez vos compétences en ingénierie informatique avec des cours modernes et complets.",
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,17 @@ export default function Home() {
       <div className="lg:w-5/6 mx-auto pt-5 px-5 h-full">
         <section className="flex justify-center flex-col h-5/6">
           <h1 className="text-white text-4xl mb-4 uppercase font-bold">
-             Boostez votre carrière de développeur
+            Boostez votre carrière de développeur
           </h1>
-          <h2 className="text-white text-2xl">
-            Avec des cours en ligne modernes
+          <h2 className="text-white text-2xl mb-4">
+            Avec des cours en ligne modernes - Bientôt en béta ouverte !
           </h2>
-          <p>Bientôt en béta ouverte !</p>
-          <a href="https://beta.nx.academy/">Découvrir le projet</a>
+          <a
+            className="bg-blue-600 hover:bg-blue-800 duration-150 h-12 w-48 flex justify-center items-center text-white"
+            href="https://beta.nx.academy/"
+          >
+            Découvrir le projet
+          </a>
         </section>
       </div>
     </main>
