db29a24d812b315fdf6418779b990681a6ed1aea
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-08-12 13:41:25 +0200

    Working on the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 export default function Home() {
   return (
-    <main className="bg-black h-screen w-screen">
-      <div className="max-w-screen-lg mx-auto pt-5 px-5 h-full">
-        <section className="flex justify-center items-center flex-col h-5/6">
-          <h1 className="text-white text-4xl mb-8 text-center uppercase">
-            Passez au niveau <br />
-            supérieur
+    <main className="h-screen w-screen bg-[url('/images/linux-programmer-pixel-art.jpg')] bg-center bg-cover">
+      <div className="lg:w-5/6 mx-auto pt-5 px-5 h-full">
+        <section className="flex justify-center flex-col h-5/6">
+          <h1 className="text-white text-4xl mb-4 uppercase font-bold">
+             Boostez votre carrière de développeur
           </h1>
-          <h2 className="text-white text-xl text-center">
-            Améliorez vos Compétences en Ingénierie Informatique
-            <br />
-            avec des Cours Modernes et Complets.
+          <h2 className="text-white text-2xl">
+            Avec des cours en ligne modernes
           </h2>
+          <p>Bientôt en béta ouverte !</p>
+          <a href="https://beta.nx.academy/">Découvrir le projet</a>
         </section>
       </div>
     </main>
