8f4d014b158af8216ed5f57a6f7a8ad8f843b2dd
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-08-12 13:51:07 +0200

    Release (#6)
    * Readding CNAME
    
    * (Feat) Perfecting landing page (#5)
    
    * Working on the landing page
    
    * Finishing basic landing (before hodilays!)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 export default function Home() {
   return (
-    <main className="bg-black h-screen w-screen">
-      <div className="max-w-screen-lg mx-auto pt-5 px-5 h-full">
-        <section className="flex justify-center items-center flex-col h-5/6">
-          <h1 className="text-white text-4xl mb-8 text-center uppercase">
-            Passez au niveau <br />
-            supérieur
+    <main className="h-screen w-screen bg-[url('/images/linux-programmer-pixel-art.jpg')] bg-center bg-cover">
+      <div className="lg:w-5/6 mx-auto pt-5 px-5 h-full">
+        <section className="flex justify-center flex-col h-5/6">
+          <h1 className="text-white text-4xl mb-4 uppercase font-bold">
+            Boostez votre carrière de développeur
           </h1>
-          <h2 className="text-white text-xl text-center">
-            Améliorez vos Compétences en Ingénierie Informatique
-            <br />
-            avec des Cours Modernes et Complets.
+          <h2 className="text-white text-2xl mb-4">
+            Avec des cours en ligne modernes - Bientôt en béta ouverte !
           </h2>
+          <a
+            className="bg-blue-600 hover:bg-blue-800 duration-150 h-12 w-48 flex justify-center items-center text-white"
+            href="https://beta.nx.academy/"
+          >
+            Découvrir le projet
+          </a>
         </section>
       </div>
     </main>
