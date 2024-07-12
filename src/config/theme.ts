13a1a87a953558af119592db30164facff1521c9
tdimnet <thomas.dimnet@gmail.com>
2024-07-12 15:52:16 +0200

    Trying to maximize astro usage

---
+++ src/config/theme.ts
@@ -0,0 +1,10 @@
+const theme = {
+    colors: {
+        black: "#151515",
+        neonGreen: "#4effa0",
+        neonGreenLight: "#4effa0aa",
+        white: "#FEFEFE"
+    }
+}
+
+export default theme
