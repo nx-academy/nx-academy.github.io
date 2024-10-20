14f015104b838dc003ff7258106494ec53632d70
tdimnet <thomas.dimnet@gmail.com>
2024-10-20 16:31:47 +0200

    Successfuly created the island

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
+
 import { firstQuiz } from "../data/quiz";
 
 export default function Component() {
+  const [selectedAnswer, setSelectedAnswer] = useState("");
   const firstQuestion = firstQuiz[0];
 
   function onSelectOption(option: string) {
-    console.log("=====")
-    console.log(option)
-    console.log("=====")
+    console.log("=====");
+    console.log(option);
+    console.log("=====");
+
+    setSelectedAnswer(option);
   }
 
   function onValidateAnswer() {
-    console.log("===")
-    console.log("===")
-    console.log("===")
+    console.log("===");
+    console.log(selectedAnswer);
+    console.log("===");
+
+    if (selectedAnswer === firstQuestion.answer) {
+      console.log("Ok, let's go!");
+    } else {
+      console.log("Nope!");
+    }
   }
 
   return (
@@ -23,7 +34,9 @@ export default function Component() {
       </div>
       <ul>
         {firstQuestion.options.map((option) => (
-          <li onClick={() => console.log(option)} key={option}>{option}</li>
+          <li onClick={() => onSelectOption(option)} key={option}>
+            {option}
+          </li>
         ))}
 
         <li onClick={() => onValidateAnswer()}>Valider la réponse</li>
