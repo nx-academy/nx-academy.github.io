8d654fcba98a9d2359df4c2e9bf79f654ce969d8
tdimnet <thomas.dimnet@gmail.com>
2024-10-23 19:49:04 +0200

    Now handling when changing question and displaying the answer

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -4,8 +4,9 @@ import "../styles/quiz.css";
 import { firstQuiz } from "../data/quiz";
 
 export default function Component() {
-  let [currentQuestion, setCurrentQuestion] = useState(0)
+  let [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState("");
+  let [isAnswerValidated, setIsAnswerValidated] = useState(false);
 
   function onSelectOption(option: string) {
     console.log("=====");
@@ -21,36 +22,59 @@ export default function Component() {
     console.log("===");
 
     if (selectedAnswer.length) {
-      if (selectedAnswer === firstQuiz[currentQuestion].answer) {
-        console.log("Ok, let's go!");
-        setCurrentQuestion(currentQuestion + 1)
-      } else {
-        console.log("Nope!");
-      }
+      setIsAnswerValidated(true);
     }
+  }
+
+  function onMoveToNextQuestion() {
+    setIsAnswerValidated(false);
+    setSelectedAnswer("");
+    setCurrentQuestion(currentQuestion + 1);
   }
 
   return (
     <div className="quiz-wrapper">
       <div className="question-wrapper">
         <p className="question-number">Question 1 sur 10</p>
-        <p className="current-question">{firstQuiz[currentQuestion].question}</p>
+        <p className="current-question">
+          {firstQuiz[currentQuestion].question}
+        </p>
       </div>
-      <ul className="answers-wrapper">
-        {firstQuiz[currentQuestion].options.map((option) => (
-          <li
-            className={`answer-btn ${selectedAnswer === option ? "active" : ""}`}
-            onClick={() => onSelectOption(option)}
-            key={option}
-          >
-            {option}
-          </li>
-        ))}
+      {isAnswerValidated ? (
+        <div>
+          {selectedAnswer === firstQuiz[currentQuestion].answer ? (
+            <p>C'est correct !</p>
+          ) : (
+            <div>
+              <p>C'est faux !</p>
+              <p>La bonne réponse était </p>
+              <p>{firstQuiz[currentQuestion].answer}</p>
+            </div>
+          )}
 
-        <li className="validation-btn" onClick={() => onValidateAnswer()}>
-          Valider la réponse
-        </li>
-      </ul>
+          <p onClick={() => onMoveToNextQuestion()} className="validation-btn">
+            Passer à la prochaine question
+          </p>
+        </div>
+      ) : (
+        <ul className="answers-wrapper">
+          {firstQuiz[currentQuestion].options.map((option) => (
+            <li
+              className={`answer-btn ${
+                selectedAnswer === option ? "active" : ""
+              }`}
+              onClick={() => onSelectOption(option)}
+              key={option}
+            >
+              {option}
+            </li>
+          ))}
+
+          <li className="validation-btn" onClick={() => onValidateAnswer()}>
+            Valider la réponse
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
