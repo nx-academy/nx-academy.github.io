eddeb6784d67438cdad5a6b139aecfdc388474e9
tdimnet <thomas.dimnet@gmail.com>
2025-04-04 18:14:43 +0200

    Making the breadcrumb utils works

--- src/utils/handleLinks/index.test.ts
+++ src/utils/handleLinks/index.test.ts
@@ -2,8 +2,23 @@ import { it, expect, describe } from "vitest"
 
 import { handleLinks } from "./index"
 
+
+const DOCKER_COURSE_URL = "/cours/docker-et-docker-compose/chapitres/installation-et-configuration-docker"
+const CI_CD_COURSE_URL = "/cours/ci-cd-github-actions/chapitres/decouverte-concepts-ci-cd"
+
+
 describe("handleLinks unit tests suite", () => {
     it("should return something", () => {
-        expect(handleLinks("/cours/docker-et-docker-compose/chapitres")).toBeDefined()
+        expect(handleLinks(CI_CD_COURSE_URL)).toBeDefined()
+    })
+
+    it("should return an object Links shape", () => {
+        expect(handleLinks(DOCKER_COURSE_URL)).toHaveProperty("previousFullChapterLink")
+        expect(handleLinks(DOCKER_COURSE_URL)).toHaveProperty("nextFullChapterLink")
+        expect(handleLinks(DOCKER_COURSE_URL)).toHaveProperty("breadcrumbs")
+    })
+
+    it("should return the correct previousFullChapterLink", () => {
+        expect(true).toBe(true)
     })
 })
--- src/utils/handleLinks/index.ts
+++ src/utils/handleLinks/index.ts
@@ -1,13 +1,42 @@
 import type { Breadcrumb } from "../../types/Breadcrumb"
 
 type Links = {
-    previousNextChapterLink: string
-    // fullNextChapterLink: string
-    // breadcrumbs: Breadcrumb[]
+    baseUrl: string
+    breadcrumbs: Breadcrumb[]
 }
 
-const BASE_URL = "/cours/docker-et-docker-compose/chapitres"
+export function handleLinks(url: string): Links {
+    let baseUrl = ""
+    let pageName = ""
 
-export function handleLinks(url: string) {
-    return url    
+    const parseToArray = url.split("/")
+
+    if (parseToArray.includes("docker-et-docker-compose")) {
+        baseUrl = "/cours/docker-et-docker-compose"
+        pageName = "Conteneurisez vos applications avec Docker"
+    } else if (parseToArray.includes("ci-cd-github-actions")) {
+        baseUrl = "/cours/ci-cd-github-actions"
+        pageName = "Maîtrisez les pipelines CI/CD avec les GitHub Actions"
+    } else {
+        throw new Error("Unknow course")
+    }
+
+
+    return {
+        baseUrl,
+        breadcrumbs: [
+            {
+                pageUrl: "/",
+                pageName: "NX Academy"
+            },
+            {
+                pageUrl: "/cours",
+                pageName: "Cours"
+            },
+            {
+                pageUrl: baseUrl,
+                pageName: pageName
+            }
+        ]
+    }
 }
