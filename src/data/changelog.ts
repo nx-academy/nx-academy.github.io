f88553f34864181432c64eaea1204821c8b197af
Thomas Dimnet <thomas.dimnet@gmail.com>
2025-04-17 14:54:47 +0200

    Adding a better format for logs

---
+++ src/data/changelog.ts
@@ -0,0 +1,83 @@
+type Task = {
+    kind: "done" | "in-progress" | "fix"
+    content: string
+}
+
+type DayLog = {
+    date: string
+    tasks: Task[]
+}
+
+type MonthLog = {
+    monthName: string,
+    dailyLogs: DayLog[]
+}
+
+const april: MonthLog = {
+    monthName: "Avril 2025",
+    dailyLogs: [
+        {
+            date: "12/04/2025",
+            tasks: [
+                {
+                    kind: "done",
+                    content: `Nouvelle fiche technique : “Comprendre les réseaux Docker”`
+                },
+                {
+                    kind: "in-progress",
+                    content: `Correction de liens cassés sur la fiche CI/CD`
+                },
+                {
+                    kind: "fix",
+                    content: `Intégration du projet RAG pour générer des quiz (phase bêta)`
+                },
+            ]
+        },
+        {
+            date: "05/04/2025",
+            tasks: [
+                {
+                    kind: "done",
+                    content: `Nouvelle fiche technique : “Comprendre les réseaux Docker”`
+                },
+                {
+                    kind: "in-progress",
+                    content: `Correction de liens cassés sur la fiche CI/CD`
+                },
+                {
+                    kind: "fix",
+                    content: `Intégration du projet RAG pour générer des quiz (phase bêta)`
+                },
+            ]
+        }
+    ]
+}
+
+const march: MonthLog = {
+    monthName: "Mars 2025",
+    dailyLogs: [
+        {
+            date: "29/03/2025",
+            tasks: [
+                {
+                    kind: "done",
+                    content: `Nouvelle fiche technique : “Comprendre les réseaux Docker”`
+                },
+                {
+                    kind: "in-progress",
+                    content: `Correction de liens cassés sur la fiche CI/CD`
+                },
+                {
+                    kind: "fix",
+                    content: `Intégration du projet RAG pour générer des quiz (phase bêta)`
+                },
+            ]
+        }
+    ]
+}
+
+
+export const ALL_MONTHS = [
+    april,
+    march
+]
