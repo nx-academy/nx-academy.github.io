5b2a376895835a064c32b4838758217b619f2b0e
Thomas Dimnet <thomas.dimnet@gmail.com>
2025-04-24 16:09:57 +0200

    Confiruging sitemap plugin (thx Astro)

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: "https://nx.academy",
+  integrations: [react(), 
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date()
+    }
+  )]
 });
