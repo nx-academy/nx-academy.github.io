9638fc54a27dea484e111c7ded58fb17bc439a50
Thomas Dimnet <thomas.dimnet@gmail.com>
2025-04-24 11:53:20 +0200

    (Feat) Recap popin (#65)
    * Starting to build the popin logic
    
    * Now able to render "conditionnal" snackbar
    
    * Styling the page and microcopying the labels
    
    * Finishing adding snackbar (and it looks SUPBER!)
    
    * Updating changelog and docs

---
+++ src/components/ResumeSnackbar.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+
+import "../styles/resume-snackbar.css";
+
+function Component() {
+  const [isSnackbarVisible, setIsSnackbarVisible] = useState(false);
+  const [lastPageVisited, setLastPageVisited] = useState(null);
+  const [lastPageLabel, setLastPageLabel] = useState(null);
+
+  useEffect(() => {
+    const currentPath = window.location.pathname;
+    const rawTitle = document.title;
+    const pageLabel = rawTitle.split(" - ")[0].trim();
+
+    const lastVisited = localStorage.getItem("lastVisited");
+    const lastVisitedLabel = localStorage.getItem("lastVisitedLabel");
+    const sessionMarker = sessionStorage.getItem("sessionStarted");
+
+    const isReturningToHome = currentPath === "/";
+    const isDifferentPage = lastVisited && lastVisited !== currentPath;
+    const isNewSession = !sessionMarker;
+
+    // If it's a new session, we set an item in sessionStorage (and not localStorage)
+    if (isNewSession) {
+      sessionStorage.setItem("sessionStarted", "true");
+    }
+
+    // We display the Snackbar, only if
+    // - the user on the home page (/)
+    // - the session just started
+    // - there is a "history", e.g. a page to back to
+    if (isReturningToHome && isNewSession && isDifferentPage) {
+      setIsSnackbarVisible(true);
+      setLastPageVisited(lastVisited);
+      setLastPageLabel(lastVisitedLabel);
+
+      const timer = setTimeout(() => {
+        setIsSnackbarVisible(false);
+      }, 10000);
+      return () => clearTimeout(timer);
+    } else {
+      setIsSnackbarVisible(false);
+    }
+
+    localStorage.setItem("lastVisited", currentPath);
+    localStorage.setItem("lastVisitedLabel", pageLabel);
+  }, []);
+
+  if (!isSnackbarVisible) return null;
+
+  return (
+    <div className="resume-snackbar-wrapper">
+      <p>
+        Vous étiez en train de lire :
+        <br />
+        <span title={lastPageLabel} className="resume-snackbar-lastpage">
+          {lastPageLabel}
+        </span>
+      </p>
+      <div className="resume-snackbar-actions">
+        <a href={lastPageVisited}>Reprendre</a>
+        <button onClick={() => setIsSnackbarVisible(false)}>
+          Pas maintenant
+        </button>
+      </div>
+      <div className="progress-bar"></div>
+    </div>
+  );
+}
+
+export default Component;
