6aa2d073ee3cac649ceafce79cff00a79dd0c2ef
tdimnet <thomas.dimnet@gmail.com>
2025-05-07 16:52:59 +0200

    Installing MDX and starting to use videos from S3 Bucket

--- astro.config.ts
+++ astro.config.ts
@@ -5,15 +5,16 @@ import react from '@astrojs/react';
 
 import sitemap from '@astrojs/sitemap';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://nx.academy",
-  integrations: [react(), 
-    sitemap({
-      filter: (page) => !page.startsWith("/drafts"),
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date()
-    }
-  )]
+  integrations: [react(), sitemap({
+    filter: (page) => !page.startsWith("/drafts"),
+    changefreq: 'weekly',
+    priority: 0.7,
+    lastmod: new Date()
+  }
+), mdx()]
 });
