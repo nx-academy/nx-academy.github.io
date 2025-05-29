91e5b9674c4fe293ac93578b340af1a5d6462a38
tdimnet <thomas.dimnet@gmail.com>
2025-05-29 11:29:16 +0200

    Create props type for the Testimonial component and an arrray of objects to rule them

---
+++ src/data/testimonials.ts
@@ -0,0 +1,27 @@
+type Testimonial = {
+    content: string
+    job: string
+    name: string
+    picture: string
+}
+
+export const TESTIMONIALS: readonly Testimonial[] = [
+    {
+        content: "This journey has been a rewarding dive into the principles of user experience, from user-centered design to interaction. Every step deepened my appreciation for creating designs that not only look good but also feel right for users.",
+        job: "Développeur front-end",
+        name: "Lionel Vozi",
+        picture: "thomas.png"
+    },
+    {
+        content: "This journey has been a rewarding dive into the principles of user experience, from user-centered design to interaction. Every step deepened my appreciation for creating designs that not only look good but also feel right for users.",
+        job: "Développeur front-end",
+        name: "Lionel Vozi",
+        picture: "thomas.png"
+    },
+    {
+        content: "This journey has been a rewarding dive into the principles of user experience, from user-centered design to interaction. Every step deepened my appreciation for creating designs that not only look good but also feel right for users.",
+        job: "Développeur front-end",
+        name: "Lionel Vozi",
+        picture: "thomas.png"
+    }
+]
