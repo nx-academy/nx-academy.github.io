d872a35b53e16874b948d8ba3dfa08dbe50fe955
tdimnet <thomas.dimnet@gmail.com>
2025-06-03 09:55:16 +0200

    Add Warning text

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -10,21 +10,21 @@ export const TESTIMONIALS: readonly Testimonial[] = [
     content:
       "This journey has been a rewarding dive into the principles of user experience, from user-centered design to interaction. Every step deepened my appreciation for creating designs that not only look good but also feel right for users.",
     job: "Développeur front-end",
-    name: "Lionel Vozi",
+    name: "Marc Strong",
     picture: "thomas.png",
   },
   {
     content:
       "This journey has been a rewarding dive into the principles of user experience, from user-centered design to interaction. Every step deepened my appreciation for creating designs that not only look good but also feel right for users.",
     job: "Développeur front-end",
-    name: "Lionel Vozi",
+    name: "Bill Gates",
     picture: "thomas.png",
   },
   {
     content:
       "This journey has been a rewarding dive into the principles of user experience, from user-centered design to interaction. Every step deepened my appreciation for creating designs that not only look good but also feel right for users.",
     job: "Développeur front-end",
-    name: "Lionel Vozi",
+    name: "Alicia Milano",
     picture: "thomas.png",
   },
 ];
