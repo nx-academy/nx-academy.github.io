6f714157a1e9005d1c5dda5abac8b054f7af55da
tdimnet <thomas.dimnet@gmail.com>
2025-06-04 10:26:42 +0200

    Write tests

--- src/utils/suffleQuestions/index.ts
+++ src/utils/suffleQuestions/index.ts
@@ -1,3 +1,12 @@
-export function suffleQuestions(questions: any[]): any[] {
-    return []
+import type { Question } from "../../types/Question"
+
+export function suffleQuestions(questions: Question[]): Question[] {
+    const shuffled = questions.slice()
+
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+
+    return shuffled
 }
