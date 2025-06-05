cd1e47d6a939abda6d1d5b5ef9fc8c2aebd8e22e
Thomas Dimnet <thomas.dimnet@gmail.com>
2025-06-05 08:56:56 +0200

    (Feat) Shared image on social media (#131)
    * Update shared image
    
    * Run Prettier
    
    * Update changelog

--- src/data/changelog.ts
+++ src/data/changelog.ts
@@ -12,6 +12,11 @@ const juneTasks: LogTasks = {
   month: "Juin",
   tasks: [
     {
+      kind: "fix",
+      content:
+        "Correction d'un bug lié au partage d'articles sur les réseaux sociaux.",
+    },
+    {
       kind: "in-progress",
       content: `Redesign de la home d'un cours. Pour l'instant, je travaille sur la page de présentation <a href="/cours/docker-et-docker-compose" target="_blank">du cours sur Docker et docker compose</a>. Une fois finalisé, je vais créer des composants dédiés puis je m'attaquerais à celle du cours sur les CI/CD.`,
     },
