a34bc12cf3fd78f7c25ce35afd869ba24f750383
Thomas Dimnet <thomas.dimnet@gmail.com>
2025-06-11 10:57:21 +0200

    (Feat) Toggle theme with button (#155)
    * Users can now change theme on the fly
    
    * Run prettier and delete useless files
    
    * Run prettier

--- src/data/changelog.ts
+++ src/data/changelog.ts
@@ -12,6 +12,11 @@ const juneTasks: LogTasks = {
   month: "Juin",
   tasks: [
     {
+      kind: "done",
+      content:
+        "Ajout d'un bouton pour changer de thème (et le garder en mémoire).",
+    },
+    {
       kind: "fix",
       content:
         "Style de la sticky outline sur les articles, fiches techniques et chapitres de cours. Pour info, c'est la fenête à droite qui donne des informations sur le contenu du cours.",
