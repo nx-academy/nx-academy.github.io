6b74ab3eeb318bd9be57c4f236fa058c8b8e7279
tdimnet <thomas.dimnet@gmail.com>
2025-08-14 10:53:51 +0200

    Run prettier

--- src/data/news.ts
+++ src/data/news.ts
@@ -1,23 +1,26 @@
 type News = {
-    content: string
-    date: string
-    title: string
-}
+  content: string;
+  date: string;
+  title: string;
+};
 
 export const NEWS: News[] = [
-    {
-        title: "Nouveau projet FrontEnd Mentor",
-        content: "FrontEnd Mentor vient de sortir un nouveau projet gratuit. Il vous propose de réaliser une application de pourboire en React Native",
-        date: "08/14/2025"
-    },
-    {
-        title: "Nouveaux cours IA sur frontend masters",
-        content: "FrontEnd Masters vient de publibier 3 nouveaux cours en rapport avec l'IA et le développement web : cours 1, cours 2 et cours 3.",
-        date: "08/08/2025"
-    },
-    {
-        title: "MAJ GPT",
-        content: "OpenIA vient de mettre à jour ChatGPT en le passant de la version 4o à la version 5. Pour l'instant, les retours ne sont pas forcément positifs",
-        date: "08/01/2025"
-    }
-]
+  {
+    title: "Nouveau projet FrontEnd Mentor",
+    content:
+      "FrontEnd Mentor vient de sortir un nouveau projet gratuit. Il vous propose de réaliser une application de pourboire en React Native",
+    date: "08/14/2025",
+  },
+  {
+    title: "Nouveaux cours IA sur frontend masters",
+    content:
+      "FrontEnd Masters vient de publibier 3 nouveaux cours en rapport avec l'IA et le développement web : cours 1, cours 2 et cours 3.",
+    date: "08/08/2025",
+  },
+  {
+    title: "MAJ GPT",
+    content:
+      "OpenIA vient de mettre à jour ChatGPT en le passant de la version 4o à la version 5. Pour l'instant, les retours ne sont pas forcément positifs",
+    date: "08/01/2025",
+  },
+];
