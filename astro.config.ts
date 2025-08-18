83c550ed9c306cda3e216cfdf917373e9f11b86f
tdimnet <thomas.dimnet@gmail.com>
2025-08-18 09:12:01 +0200

    Set Up Astro DB

--- astro.config.ts
+++ astro.config.ts
@@ -5,16 +5,15 @@ import react from "@astrojs/react";
 
 import sitemap from "@astrojs/sitemap";
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://nx.academy",
-  integrations: [
-    react(),
-    sitemap({
-      filter: (page) => !page.startsWith("/drafts"),
-      changefreq: "weekly",
-      priority: 0.7,
-      lastmod: new Date(),
-    }),
-  ],
+  integrations: [react(), sitemap({
+    filter: (page) => !page.startsWith("/drafts"),
+    changefreq: "weekly",
+    priority: 0.7,
+    lastmod: new Date(),
+  }), db()],
 });
