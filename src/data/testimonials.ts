aec7baa96e61a708da7e03a1bba108b863729d32
Thomas Dimnet <thomas.dimnet@gmail.com>
2025-10-18 10:20:34 +0200

    Updating testimonials

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -3,21 +3,21 @@ import type { Testimonial } from "../types/Testimonial";
 export const TESTIMONIALS: readonly Testimonial[] = [
   {
     content:
-      "Novice avec Docker, j'ai pu rapidement appréhender les concepts clés grâce à NX. Grâce à la pédagogie de Thomas, les explications claires et exercices, mes connaissances sur cet outil ont pu être approfondies. Je suis désormais parfaitement à l'aise avec Docker.",
+      "Novice avec Docker, j’ai pu rapidement comprendre les concepts essentiels grâce à NX. La pédagogie de Thomas, ses explications claires et les exercices pratiques m’ont permis de progresser pas à pas. Aujourd’hui, je me sens vraiment à l’aise avec Docker.",
     job: "Développeur JavaScript / Python",
     name: "Loïc",
     picture: "/images/cours-docker-et-docker-compose/loic.webp",
   },
   {
     content:
-      "J'avais déjà des bases sur Docker. Grâce au cours de NX, j’ai enfin pu structurer ma compréhension. L'approche de NX m’a permis de consolider mon modèle mental : je comprends désormais comment les différentes briques de Docker s’articulent et pourquoi on les utilise. Une vraie montée en clarté.",
+      "J’avais déjà quelques bases sur Docker, mais le cours NX m’a permis de mettre de l’ordre dans mes connaissances. Grâce à l’approche claire et progressive de Thomas, j’ai enfin compris comment les différentes briques de Docker s’articulent entre elles et surtout pourquoi on les utilise ! Une vraie mise en lumière.",
     job: "Développeur Full Stack",
     name: "Yacine",
     picture: "/images/cours-docker-et-docker-compose/yacine.webp",
   },
   {
     content:
-      "Ce cours m’a permis d’aller bien plus vite dans ma montée en compétences sur Docker. Au-delà des explications claires de Thomas, la richesse des ressources partagées (schémas, articles, vidéos, exercices) m’a vraiment aidé à mieux comprendre et à ancrer les concepts. J’ai gagné en autonomie.",
+      "Ce cours m’a vraiment aidé à accélérer ma montée en compétences sur Docker. Les explications de Thomas, les schémas et les exercices m’ont permis de mieux comprendre les concepts et de les ancrer durablement. Aujourd’hui, je me sens beaucoup plus autonome.",
     job: "Développeuse Back End",
     name: "Leslie",
     picture: "/images/cours-docker-et-docker-compose/leslie.webp",
